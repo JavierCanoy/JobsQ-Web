9c1e8ba94a7bd2748783e45f3cf2a5e63895cd1b
JavierCanoy <javier@mabolotech.com>
2023-08-10 09:32:08 +0800

    d1

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ import Main from "../components/Main";
 export default function Home() {
   return (
     <div className="homage-page">
-      https://app.slack.com/ssb/signin?ssb_vid=.b242426456d6616543df7c5372067807&ssb_instance_id=b2e91f18-fec5-4036-b341-1031a34cf255&v=4.33.84&from_desktop_app=1#/signin{" "}
       <Head>
         <title>Mabolotecth-Solution</title>
         <link rel="icon" href="/logo-web.png" />
